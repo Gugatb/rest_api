00b662bd9565d4aa4c75296ff7979a06a0a112ed
Gustavo Tsuda Bellentani <gugatb@gmail.com>
2023-01-14 09:59:47 -0300

    Removing the comments

--- Python/app.py
+++ Python/app.py
@@ -8,30 +8,14 @@ api = Api(app)
 
 @app.route('/hello')
 def print_hello():
-    '''
-    Exibir o ola mundo.
-    Author: Gugatb
-    Date: 15/06/2018
-    '''
-    return jsonify('Ola mundo')
+    return jsonify('Hello world!')
 
 @app.route('/param')
 def print_params():
-    '''
-    Exibir os parametros.
-    Author: Gugatb
-    Date: 15/06/2018
-    '''
     return jsonify({'key1': request.args.get('key1'), 'key2': request.args.get('key2')})
 
 @app.route('/text/<text>')
 def print_text(text):
-    '''
-    Imprimir a informacao.
-    Author: Gugatb
-    Date: 23/07/2018
-    Param: information a informacao
-    '''
     return jsonify({'text': text})
 
 if __name__ == '__main__':
--- Ruby/app.rb
+++ Ruby/app.rb
@@ -1,27 +1,16 @@
 
 require 'sinatra'
 
-# Definir a porta.
 set :port, 8080
 
-# Exibir o ola mundo.
-# Author: Gugatb
-# Date: 17/06/2018
 get '/hello' do
-  'Ola mundo'
+  'Hello world!'
 end
 
-# Exibir os parametros.
-# Author: Gugatb
-# Date: 17/06/2018
 get '/param' do
   {'key1' => params['key1'], 'key2' => params['key2']}.to_json
 end
 
-# Exibir o texto.
-# Author: Gugatb
-# Date: 15/06/2018
-# Param: value o valor
 get '/text/:text' do
   {'text' => params[:text]}.to_json
 end
